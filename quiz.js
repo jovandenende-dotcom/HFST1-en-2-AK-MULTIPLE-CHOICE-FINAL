6bac0a2f33b90878c72fa723d7bf99fcf4e1b43c
jovandenende-dotcom <jo.vandenende@gmail.com>
2026-01-01 21:50:45 +0100

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -41,6 +41,7 @@ const questions = [
   }
 ];
 
+// ---- QUIZ LOGICA ----
 function shuffle(array) {
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
