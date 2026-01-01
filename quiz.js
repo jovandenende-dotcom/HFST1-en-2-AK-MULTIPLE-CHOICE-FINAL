f5ff895124d82a5571c18c070724c346c2567d9c
jovandenende-dotcom <jo.vandenende@gmail.com>
2026-01-01 22:41:50 +0100

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -249,6 +249,256 @@ const questions = [
   ],
   answer: 1
 }
+{
+  question: "Wat is de atmosfeer?",
+  options: [
+    "Het vaste deel van de aarde",
+    "Het geheel aan gasvormige stoffen die de aarde omringen",
+    "Alle water op aarde",
+    "De aardmantel"
+  ],
+  answer: 1
+},
+{
+  question: "Wat is de benedenloop van een rivier?",
+  options: [
+    "Het deel bij de bron",
+    "Het deel met groot verval",
+    "Het deel vanaf de middenloop tot de monding met geringe stroomsnelheid",
+    "Het deel waar vooral erosie plaatsvindt"
+  ],
+  answer: 2
+},
+{
+  question: "Wat is de biosfeer?",
+  options: [
+    "Alle water op aarde",
+    "Het leven op aarde",
+    "De aardkorst",
+    "De atmosfeer"
+  ],
+  answer: 1
+},
+{
+  question: "Wat is de bovenloop van een rivier?",
+  options: [
+    "Het deel met weinig verval",
+    "Het deel vanaf de bron tot de middenloop met groot verval en erosie",
+    "Het deel bij de monding",
+    "Het deel waar sedimentatie overheerst"
+  ],
+  answer: 1
+},
+{
+  question: "Wat is chemische verwering?",
+  options: [
+    "Uiteenvallen van gesteente zonder samenstellingsverandering",
+    "Uiteenvallen van gesteente waarbij de scheikundige samenstelling verandert",
+    "Afzetting van sediment",
+    "Transport van gesteente"
+  ],
+  answer: 1
+},
+{
+  question: "Wat is condensatie?",
+  options: [
+    "Verdamping van water",
+    "Vorming van waterdruppels door afkoelen van waterdamp",
+    "Neerslag uit wolken",
+    "Transport van water"
+  ],
+  answer: 1
+},
+{
+  question: "Wat is een delta?",
+  options: [
+    "Steile kust",
+    "Kust die ontstaat bij de monding van een vertakte rivier door afzetting",
+    "Onderzeese kloof",
+    "Kust gevormd door afbraak"
+  ],
+  answer: 1
+},
+{
+  question: "Wat is een eindmorene?",
+  options: [
+    "Door rivier afgezet materiaal",
+    "Heuvelrug van materiaal dat een gletsjer voor zich uit heeft geschoven",
+    "Sediment in zee",
+    "Puinhelling"
+  ],
+  answer: 1
+},
+{
+  question: "Wat is de hydrologische kringloop?",
+  options: [
+    "De kringloop van magma",
+    "De kringloop van water via verdamping, condensatie, neerslag en transport",
+    "De kringloop van gesteenten",
+    "De kringloop van lucht"
+  ],
+  answer: 1
+},
+{
+  question: "Wat is kalksteen?",
+  options: [
+    "Stollingsgesteente",
+    "Gesteente dat ontstaat uit kalkhoudende resten van zee-organismen",
+    "Metamorf gesteente",
+    "Gesteente uit lava"
+  ],
+  answer: 1
+},
+{
+  question: "Wat is een karstlandschap?",
+  options: [
+    "Landschap gevormd door gletsjers",
+    "Landschap in kalksteen door het oplossen van kalk in water",
+    "Woestijnlandschap",
+    "Vulkanisch landschap"
+  ],
+  answer: 1
+},
+{
+  question: "Wat is een klifkust?",
+  options: [
+    "Kust waarbij afzetting overheerst",
+    "Steile kust ontstaan door afbraak door de zee",
+    "Kust met duinen",
+    "Kust gevormd door breuken"
+  ],
+  answer: 1
+},
+{
+  question: "Wat is leisteen?",
+  options: [
+    "Stollingsgesteente",
+    "Grijs of zwart metamorf gesteente ontstaan uit schalie",
+    "Sedimentgesteente",
+    "Vulkanisch gesteente"
+  ],
+  answer: 1
+},
+{
+  question: "Wat is de lithosfeer?",
+  options: [
+    "De aardmantel",
+    "De buitenste schil van de aarde, bestaande uit aardkorst en vaste bovenmantel",
+    "De aardkern",
+    "De atmosfeer"
+  ],
+  answer: 1
+},
+{
+  question: "Wat is marmer?",
+  options: [
+    "Sedimentgesteente",
+    "Metamorf gesteente ontstaan uit kalksteen",
+    "Stollingsgesteente",
+    "Gesteente uit lava"
+  ],
+  answer: 1
+},
+{
+  question: "Wat is een massabeweging?",
+  options: [
+    "Beweging van aardplaten",
+    "Verplaatsing van verweringsmateriaal door zwaartekracht",
+    "Transport door wind",
+    "Afzetting van sediment"
+  ],
+  answer: 1
+},
+{
+  question: "Wat is een meander?",
+  options: [
+    "Een riviermonding",
+    "Een natuurlijke bocht in een rivier",
+    "Een stroomgebied",
+    "Een delta"
+  ],
+  answer: 1
+},
+{
+  question: "Wat is mechanische verwering?",
+  options: [
+    "Verwering waarbij de samenstelling verandert",
+    "Verwering waarbij de chemische samenstelling niet verandert",
+    "Erosie door water",
+    "Sedimentatie"
+  ],
+  answer: 1
+},
+{
+  question: "Wat is een metamorf gesteente?",
+  options: [
+    "Gesteente ontstaan door afkoeling van magma",
+    "Gesteente dat door druk en/of temperatuur andere eigenschappen heeft gekregen",
+    "Gesteente ontstaan door afzetting",
+    "Vulkanisch gesteente"
+  ],
+  answer: 1
+},
+{
+  question: "Wat is de middenloop van een rivier?",
+  options: [
+    "Het deel bij de bron",
+    "Het deel tussen boven- en benedenloop waar erosie en sedimentatie ongeveer in evenwicht zijn",
+    "Het deel bij de monding",
+    "Het deel met alleen erosie"
+  ],
+  answer: 1
+},
+{
+  question: "Wat is een morene?",
+  options: [
+    "Door rivier afgezet materiaal",
+    "Afzetting van door landijs of gletsjers aangevoerd materiaal",
+    "Puinhelling",
+    "Zandafzetting"
+  ],
+  answer: 1
+},
+{
+  question: "Wat is de bovenloop?",
+  options: [
+    "De riviermonding",
+    "Het deel met groot verval en veel erosie",
+    "Het deel met veel sedimentatie",
+    "Het deel in zee"
+  ],
+  answer: 1
+},
+{
+  question: "Wat is de benedenloop?",
+  options: [
+    "Het deel met groot verval",
+    "Het deel met geringe stroomsnelheid en veel sedimentatie",
+    "Het deel bij de bron",
+    "Het deel in de bergen"
+  ],
+  answer: 1
+},
+{
+  question: "Wat is transport door een rivier?",
+  options: [
+    "Afbraak van gesteente",
+    "Het meenemen van sediment door stromend water",
+    "Het afzetten van sediment",
+    "Chemische verwering"
+  ],
+  answer: 1
+},
+{
+  question: "Wat is sedimentatie door een rivier?",
+  options: [
+    "Het oplossen van kalksteen",
+    "Het afzetten van meegenomen sediment",
+    "Het uitschuren van de bedding",
+    "Het transport van water"
+  ],
+  answer: 1
+},
 
 ];
 
